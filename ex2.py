b0feec33444ece1483f6884f9b809ef450ba4d42
zhangchaoy <zhang1830@126.com>
2014-01-26 14:49:34 +0800

    Comments And Pound Characters

---
+++ ex2.py
@@ -0,0 +1,13 @@
+#coding=UTF-8
+
+#这些内容是要被注释掉的，不是显示在最终的输出结果里
+#A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+
+print "I could have code like this." #and the comment after is ignored
+
+#You can also use a comment to "disable" or comment out a piece of code:
+#print "This won't run."
+
+print "This will run."
+print "这些内容是要被打印出来的"
