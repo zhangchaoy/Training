b70650a0c6ac6a7edc30a77518cd022875809cf2
zhangchaoy <zhang1830@126.com>
2014-01-26 13:44:44 +0800

    A Good First Program
    输出结果：
    Hello World!
    Hello Again
    I like typing this.
    This is fun.
    Yay! Printing.
    I'd much rather you 'not'.
    I "said" do not touch this.
    你好，我来啦

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#coding=UTF-8 
+#输出语句中对单引号和双引号的使用
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "你好，我来啦"
